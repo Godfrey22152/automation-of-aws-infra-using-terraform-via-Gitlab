9906a0c829af28b1ed9489bb1e7e1ddaad6ce2f6
GODFREY IFEANYI <90422561+Godfrey22152@users.noreply.github.com>
2024-09-24 19:23:21 +0100

    Updated main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "server_encrpt" {
 }
 
 # Resource to create DynamoDB-table
-/*resource "aws_dynamodb_table" "statelock" {
+resource "aws_dynamodb_table" "statelock" {
   name           = "state-lock"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
@@ -43,4 +43,4 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "server_encrpt" {
     Name        = "dynamodb-table-1"
     Environment = "env"
   }
-}*/
+}
