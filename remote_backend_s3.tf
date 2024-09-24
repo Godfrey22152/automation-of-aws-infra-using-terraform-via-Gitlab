fe3f33034236433b0aeb281afceb3e39a8364a62
GODFREY IFEANYI <90422561+Godfrey22152@users.noreply.github.com>
2024-09-24 19:24:27 +0100

    Updated remote_backend_s3.tf

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "dev-project-remote-state-bucket-22152"
-    #dynamodb_table = "state-lock" 
+    dynamodb_table = "state-lock" 
     key            = "devops-project/terraform.tfstate"
     region         = "eu-west-1"
     encrypt        = true
